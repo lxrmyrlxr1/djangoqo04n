bcfd724274c5d399f3d9bc2c06b1edb1607cc10a
lxrmyrlxr1 <1305390596@qq.com>
2025-03-16 22:10:12 +0000

    Update apps.py

--- main/apps.py
+++ main/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
+import os
+port = int(os.getenv("PORT", 5000))  
+app.run(host="0.0.0.0", port=port)
 
 class MainConfig(AppConfig):
     name = 'main'
-    verbose_name = verbose_name_plural = r'刷表模块'
+    verbose_name = verbose_name_plural = r''
