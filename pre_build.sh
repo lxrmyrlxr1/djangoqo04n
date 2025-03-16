947b506c0b1d67c17434a833cdaeb0e3771f08c2
lxrmyrlxr1 <1305390596@qq.com>
2025-03-16 19:39:01 +0000

    Update pre_build.sh

--- pre_build.sh
+++ pre_build.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 rm -rf /opt/render/project/src/.venv
+apt-get update
+apt-get install -y openjdk-11-jdk
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+echo "export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64" >> ~/.bashrc
