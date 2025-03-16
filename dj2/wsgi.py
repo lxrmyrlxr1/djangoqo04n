44cb17105407e927127276251cf27da9a3d486d0
lxrmyrlxr1 <1305390596@qq.com>
2025-03-16 18:24:27 +0000

    Update wsgi.py

--- dj2/wsgi.py
+++ dj2/wsgi.py
@@ -10,6 +10,10 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from fix_collections import collections
+
+from django.core.wsgi import get_wsgi_application
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dj2.settings")
 
