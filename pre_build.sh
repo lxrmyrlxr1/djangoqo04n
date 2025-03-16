ad83ba61e733f16d5764dfa120cef6aa48306301
lxrmyrlxr1 <1305390596@qq.com>
2025-03-16 19:44:00 +0000

    Update pre_build.sh

--- pre_build.sh
+++ pre_build.sh
@@ -3,4 +3,7 @@ rm -rf /opt/render/project/src/.venv
 apt-get update
 apt-get install -y openjdk-11-jdk
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
-echo "export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64" >> ~/.bashrc
+echo "export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64" >> /etc/environment
+echo "export PATH=$JAVA_HOME/bin:$PATH" >> /etc/environment
+source /etc/environment
+java -version
