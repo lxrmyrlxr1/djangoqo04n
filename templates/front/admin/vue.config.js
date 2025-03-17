b663fc1d1bf2fc674aba63824f4f2190105b4e2d
lxrmyrlxr1 <1305390596@qq.com>
2025-03-17 16:53:34 +0000

    Update vue.config.js

--- templates/front/admin/vue.config.js
+++ templates/front/admin/vue.config.js
@@ -2,62 +2,62 @@
 //vue2
 const path = require('path')
 function resolve(dir) {
-    return path.join(__dirname, dir)
+return path.join(__dirname, dir)
 }
 function publicPath(){
-    if (process.env.NODE_ENV == 'production') {
-        return "././";
-    } else {
-        return "/";
-    }
+if (process.env.NODE_ENV == 'production') {
+return "././";
+} else {
+return "/";
 }
-// vue.config.js
+}
+//vue.config.js
 module.exports = {
-    // publicPath:"././",
-    publicPath: publicPath(),
-    // 国际化配置 使用其它语言，默认情况下中文语言包依old是被引入的
-    configureWebpack: {
-        // plugins: [
-        //     new webpack.NormalModuleReplacementPlugin(/element-ui[\/\\]lib[\/\\]locale[\/\\]lang[\/\\]zh-CN/, 'element-ui/lib/locale/lang/en')
-        // ]
-        resolve: {
-            alias: {
-                '@': resolve('src')
-            }
-        }
-    },
+// publicPath:"././",
+publicPath: publicPath(),
+// The international configuration uses other languages, and by default the Chinese language package is introduced by old
+configureWebpack: {
+// plugins: [
+//     new webpack.NormalModuleReplacementPlugin(/element-ui[\/\\]lib[\/\\]locale[\/\\]lang[\/\\]zh-CN/, 'element-ui/lib/locale/lang/en')
+// ]
+resolve: {
+alias: {
+'@': resolve('src')
+}
+}
+},
 lintOnSave: false,
-    devServer: {
-        host: "0.0.0.0", //指定使用一个 host。默认是 localhost，这里默认值即可
-        port: 8081, //指定端口
-        hot: true, // 开启热更new
-        https: false, // 是否开启https模式
-        proxy: { // 请求代理服务器
-            '/djangoqo04n': { //带上api前缀的
-                target: 'http://localhost:8080/djangoqo04n/', //代理目标address
-                changeOrigin: true,
-                secure: false,
-                pathRewrite: { // 在发出请求后将/api替换为''空值，这样不影响接口请求
-                    '^/djangoqo04n': ''
-                }
-            }
-        }
-    },
+devServer: {
+Host: "0.0.0.0", / / specifies to use a host. The default is localhost, but here is the default value
+Port: 8081, / / specified port
+Hot: true, / / open hot more new
+The https: false, / / whether to open the https mode
+Proxy: {/ / Request proxy server
+'/ djangoqo04n': {/ / with an api prefix
+target: 'http://localhost:8080/djangoqo04n/', //代理目标address
+changeOrigin: true,
+secure: false,
+pathRewrite: {/ / Replace / api with '' null value after making the request, which does not affect the interface request
+'^/djangoqo04n': ''
+}
+}
+}
+},
 chainWebpack(config) {
-    config.module
-      .rule('svg')
-      .exclude.add(resolve('src/icons'))
-      .end()
-    config.module
-      .rule('icons')
-      .test(/\.svg$/)
-      .include.add(resolve('src/icons'))
-      .end()
-      .use('svg-sprite-loader')
-      .loader('svg-sprite-loader')
-      .options({
-        symbolId: 'icon-[name]'
-      })
-      .end()
+config.module
+.rule('svg')
+.exclude.add(resolve('src/icons'))
+.end()
+config.module
+.rule('icons')
+.test(/\.svg$/)
+.include.add(resolve('src/icons'))
+.end()
+.use('svg-sprite-loader')
+. loader ('svg-sprite-loader ')
+.options({
+symbolId: 'icon-[name]'
+})
+.end()
 }
 }
