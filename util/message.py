7587b52f7a102f9a4258523029a58027b02e3f9e
lxrmyrlxr1 <1305390596@qq.com>
2025-03-17 16:57:21 +0000

    Update message.py

--- util/message.py
+++ util/message.py
@@ -1,27 +1,20 @@
 # coding:utf-8
 # author:ila
-normal_code = "正常"
-redirect_code = "跳转"
-temporary_redirect_code = "暂时跳转"
-
-empty_param_code = "请求参数为空或错误"
-validate_param_code = "请求参数不规范"
-is_not_json_code = "数据格式不是json"
-
-other_code = "其它错误"
-crud_error_code = "数据库operate失败"
-header_error_code = "头部错误"
-captcha_error_code = "验证码错误"
-id_exist_code = "id或记录已存在"
-id_notexist_code = "id或记录不存在"
-file_notexist_code = "上传文件不存在"
-username_error_code = "username错误"
-password_error_code = "password错误"
-
-token_error_code = "token错误"
-token_expired_code = "token错误"
-non_authorized_code = "无权限"
-
-system_error_code = "系统级错误"
-request_expired_code = "请求已过期"
-repeated_request_code = "重复请求"
+normal_code = 0 # Normal
+Empty _ param _ code = 10001 # The request parameter is empty or wrong
+validate _ param _ code = 10002 # Request parameter is not standard
+The is _ not _ json _ code = 10003 # Data format is not a json
+Oather _ code = 10020 # Other errors
+The crud _ error _ code = 10021 # Database operate failed
+header_error_code = 10023 # header error
+The id _ already _ code = 10025 # id or username already exists
+The id_notexist_code = 10026 # id or username does not exist
+username_error_code = 10027  # username error
+password_error_code = 10028  # password error
+File _ notexist _ code = 10029 # The uploaded file does not exist
+Token _ error _ code = 20001 # token error
+token_expired_code = 20002  # token erroe
+No _ authorized _ code = 20003 # No permission
+system_error_code = 40001 # System-level error
+request_expired_code = 40002 # Request has expired
+repeated_request_code = 40003
