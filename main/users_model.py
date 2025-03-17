8e6140c1d13711ee1affda7a4e952a8ee9afe88e
lxrmyrlxr1 <1305390596@qq.com>
2025-03-17 16:49:50 +0000

    Update users_model.py

--- main/users_model.py
+++ main/users_model.py
@@ -7,16 +7,16 @@ from .model import BaseModel
 
 
 class users(BaseModel):
-    # id=models.BigIntegerField(verbose_name="自增id")
-    username = models.CharField(max_length=100, verbose_name=u'账号')
+    # id=models.BigIntegerField(verbose_name="add id")
+    username = models.CharField(max_length=100, verbose_name=u'username')
     password = models.CharField(max_length=100, verbose_name=u'password')
-    role = models.CharField(max_length=100, verbose_name=u'角色')
-    addtime = models.DateTimeField(auto_now_add=False, verbose_name=u'创建时间')
+    role = models.CharField(max_length=100, verbose_name=u'adimin')
+    addtime = models.DateTimeField(auto_now_add=False, verbose_name=u'creat time')
     __tablename__ = 'users'
 
     class Meta:
         db_table = 'users'
-        verbose_name = verbose_name_plural = u'administrator表'
+        verbose_name = verbose_name_plural = u'administrator'
 
     # def __str__(self):
     #     return self.username
