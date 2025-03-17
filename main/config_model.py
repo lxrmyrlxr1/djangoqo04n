fccc40e7c9b3c58bb67b10f5da75b1faa7326b50
lxrmyrlxr1 <1305390596@qq.com>
2025-03-17 16:31:52 +0000

    Update config_model.py

--- main/config_model.py
+++ main/config_model.py
@@ -7,15 +7,15 @@ from .model import BaseModel
 
 
 class config(BaseModel):
-    # id=models.BigIntegerField(verbose_name="自增id")
-    name = models.CharField(max_length=100, verbose_name=u'键名')
-    value = models.CharField(max_length=100, verbose_name=u'键值')
+    # id=models.BigIntegerField(verbose_name="add id")
+    name = models.CharField(max_length=100, verbose_name=u'name')
+    value = models.CharField(max_length=100, verbose_name=u'value')
 
     __tablename__ = 'config'
 
     class Meta:
         db_table = 'config'
-        verbose_name = verbose_name_plural = u'配置表'
+        verbose_name = verbose_name_plural = u'table'
 
     # def __str__(self):
     #     return self.name
