dad9b790ca4135160899fce2612dfd603eb0dd3d
lxrmyrlxr1 <1305390596@qq.com>
2025-03-18 17:11:26 +0000

    Update Editor.vue

--- templates/front/admin/src/components/common/Editor.vue
+++ templates/front/admin/src/components/common/Editor.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <!-- picture上传组件辅助-->
+    <!-- picture-->
     <el-upload
       class="avatar-uploader"
       :action="getActionUrl"
@@ -24,22 +24,22 @@
   </div>
 </template>
 <script>
-// 工具栏配置
+
 const toolbarOptions = [
-  ["bold", "italic", "underline", "strike"], // 加粗 斜体 下划线 deleted线
-  ["blockquote", "code-block"], // 引用  代码块
-  [{ header: 1 }, { header: 2 }], // 1、2 级title
-  [{ list: "ordered" }, { list: "bullet" }], // 有序、无序列表
-  [{ script: "sub" }, { script: "super" }], // 上标/下标
-  [{ indent: "-1" }, { indent: "+1" }], // 缩进
-  // [{'direction': 'rtl'}],                         // 文本方向
-  [{ size: ["small", false, "large", "huge"] }], // 字体大小
+  ["bold", "italic", "underline", "strike"], 
+  ["blockquote", "code-block"],
+  [{ header: 1 }, { header: 2 }], // title
+  [{ list: "ordered" }, { list: "bullet" }], 
+  [{ script: "sub" }, { script: "super" }], 
+  [{ indent: "-1" }, { indent: "+1" }],
+  // [{'direction': 'rtl'}],                        
+  [{ size: ["small", false, "large", "huge"] }], 
   [{ header: [1, 2, 3, 4, 5, 6, false] }], // title
-  [{ color: [] }, { background: [] }], // 字体颜色、字体背景颜色
-  [{ font: [] }], // 字体种类
-  [{ align: [] }], // 对齐方式
-  ["clean"], // 清除文本格式
-  ["link", "image", "video"] // 链接、picture、视频
+  [{ color: [] }, { background: [] }], 
+  [{ font: [] }], 
+  [{ align: [] }], 
+  ["clean"], 
+  ["link", "image", "video"] 
 ];
 
 import { quillEditor } from "vue-quill-editor";
@@ -49,14 +49,12 @@ import "quill/dist/quill.bubble.css";
 
 export default {
   props: {
-    /*编辑器的context*/
     value: {
       type: String
     },
     action: {
       type: String
     },
-    /*picture大小*/
     maxSize: {
       type: Number,
       default: 4000 //kb
@@ -70,7 +68,7 @@ export default {
   data() {
     return {
       content: this.value,
-      quillUpdateImg: false, // 根据picture上传状态来confirm是否显示loading动画，刚开始是false,不显示
+      quillUpdateImg: false, 
       editorOption: {
         placeholder: "",
         theme: "snow", // or 'bubble'
@@ -81,7 +79,6 @@ export default {
             handlers: {
               image: function(value) {
                 if (value) {
-                  // 触发input框选择picture文件
                   document.querySelector(".avatar-uploader input").click();
                 } else {
                   this.quill.format("image", false);
@@ -89,7 +86,7 @@ export default {
               }
               // link: function(value) {
               //   if (value) {
-              //     var href = prompt('请输入url');
+              //     var href = prompt('url');
               //     this.quill.format("link", href);
               //   } else {
               //     this.quill.format("link", false);
@@ -103,11 +100,11 @@ export default {
       header: {
         // token: sessionStorage.token
        'Token': this.$storage.get("Token")
-      } // 有的picture服务器要求请求头需要有token
+      }
     };
   },
   computed: {
-    // 计算属性的 getter
+  
     getActionUrl: function() {
       // return this.$base.url + this.action + "?token=" + this.$storage.get("token");
       return `/${this.$base.name}/` + this.action;
@@ -115,45 +112,31 @@ export default {
   },
   methods: {
     onEditorBlur() {
-      //失去焦点事件
     },
     onEditorFocus() {
-      //获得焦点事件
     },
     onEditorChange() {
       console.log(this.value);
-      //context改变事件
       this.$emit("input", this.value);
     },
-    // 富文本picture上传前
     beforeUpload() {
-      // 显示loading动画
       this.quillUpdateImg = true;
     },
 
     uploadSuccess(res, file) {
-      // res为picture服务器返回的数据
-      // 获取富文本组件实例
       let quill = this.$refs.myQuillEditor.quill;
-      // 如果上传success
       if (res.code === 0) {
-        // 获取光标所在location
         let length = quill.getSelection().index;
-        // 插入picture  res.url为服务器返回的pictureaddress
         quill.insertEmbed(length, "image", this.$base.url+ "upload/" +res.file);
-        // 调整光标到最后
         quill.setSelection(length + 1);
       } else {
-        this.$message.error("picture插入失败");
+        this.$message.error("picture");
       }
-      // loading动画消失
       this.quillUpdateImg = false;
     },
-    // 富文本picture上传失败
     uploadError() {
-      // loading动画消失
       this.quillUpdateImg = false;
-      this.$message.error("picture插入失败");
+      this.$message.error("picture");
     }
   }
 };
@@ -164,16 +147,16 @@ export default {
   line-height: normal !important;
 }
 .ql-snow .ql-tooltip[data-mode="link"]::before {
-  content: "请输入链接address:";
+  content: "address:";
 }
 .ql-snow .ql-tooltip.ql-editing a.ql-action::after {
   border-right: 0px;
-  content: "保存";
+  content: "";
   padding-right: 0px;
 }
 
 .ql-snow .ql-tooltip[data-mode="video"]::before {
-  content: "请输入视频address:";
+  content: "address:";
 }
 .ql-container {
 	height: 400px;
@@ -227,14 +210,14 @@ export default {
 
 .ql-snow .ql-picker.ql-font .ql-picker-label::before,
 .ql-snow .ql-picker.ql-font .ql-picker-item::before {
-  content: "标准字体";
+  content: "";
 }
 .ql-snow .ql-picker.ql-font .ql-picker-label[data-value="serif"]::before,
 .ql-snow .ql-picker.ql-font .ql-picker-item[data-value="serif"]::before {
-  content: "衬线字体";
+  content: "";
 }
 .ql-snow .ql-picker.ql-font .ql-picker-label[data-value="monospace"]::before,
 .ql-snow .ql-picker.ql-font .ql-picker-item[data-value="monospace"]::before {
-  content: "等宽字体";
+  content: "";
 }
 </style>
