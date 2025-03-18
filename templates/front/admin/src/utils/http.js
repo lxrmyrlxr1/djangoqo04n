14245aa45e6a189957112cc1505230ca106ce6b4
lxrmyrlxr1 <1305390596@qq.com>
2025-03-18 17:05:08 +0000

    Update http.js

--- templates/front/admin/src/utils/http.js
+++ templates/front/admin/src/utils/http.js
@@ -10,14 +10,14 @@ const http = axios.create({
         'Content-Type': 'application/json; charset=utf-8'
     }
 })
-// 请求拦截
+
 http.interceptors.request.use(config => {
     config.headers['Token'] = storage.get('Token') // 请求头带上token
     return config
 }, error => {
     return Promise.reject(error)
 })
-// 响应拦截
+
 http.interceptors.response.use(response => {
     if (response.data && response.data.code === 401) { // 401, token失效
         router.push({ name: 'login' })
