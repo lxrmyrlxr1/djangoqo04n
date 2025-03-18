b3908df723d47201040db4fbbacea81850ada023
lxrmyrlxr1 <1305390596@qq.com>
2025-03-18 17:06:37 +0000

    Update menu.js

--- templates/front/admin/src/utils/menu.js
+++ templates/front/admin/src/utils/menu.js
@@ -1,6 +1,7 @@
 const menu = {
     list() {
-        return [{"backMenu":[{"child":[{"appFrontIcon":"cuIcon-attentionfavor","buttons":["added","查看","Revise","deleted","查看evaluate","Crawl data"],"menu":"ticket information","menuJump":"列表","tableName":"menpiaoxinxi"}],"menu":"ticket information"},{"child":[{"appFrontIcon":"cuIcon-vip","buttons":["added","查看","Revise","deleted","Crawl data"],"menu":"hotel Information","menuJump":"列表","tableName":"mingsuxinxi"}],"menu":"hotel information"},{"child":[{"appFrontIcon":"cuIcon-keyboard","buttons":["查看","Revise"],"menu":"system introduce","tableName":"systemintro"}],"menu":"system Information"}],"frontMenu":[],"hasBackLogin":"是","hasBackRegister":"否","hasFrontLogin":"否","hasFrontRegister":"否","roleName":"administrator","tableName":"users"}]
+        return [{"backMenu":[{"child":[{"appFrontIcon":"cuIcon-attentionfavor","buttons":["added","check","Revise","deleted","check evaluate","Crawl data"],"menu":"ticket information","menuJump":"list","tableName":"menpiaoxinxi"}],"menu":"ticket information"},{"child":[{"appFrontIcon":"cuIcon-vip","buttons":["added","check","Revise","deleted","Crawl data"],"menu":"hotel Information","menuJump":"list","tableName":"mingsuxinxi"}],"menu":"hotel information"},{"child":[{"appFrontIcon":"cuIcon-keyboard","buttons":["check","Revise"],"menu":"system introduce","tableName":"systemintro"}],"menu":"system Information"}],"frontMenu":[],"hasBackLogin":"yes","hasBackRegister":"no","hasFrontLogin":"no","hasFrontRegister":"no","roleName":"administrator","tableName":"users"}]
     }
 }
 export default menu;
+check
